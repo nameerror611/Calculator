798495a3aea1b0c848327c5c6fd0e243e5db694b
nameerror611 <nameerror611@gmail.com>
2026-01-04 14:44:06 +0100

    Update script.js
    ok

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ button.addEventListener("click", showMessage);
 
 // Skrót: Ctrl+Enter / Cmd+Enter
 textarea.addEventListener("keydown", (e) => {
+  const value = textarea.value.trim.lenght();
+   hint.textContent = value;
   const isMac = navigator.platform.toUpperCase().includes("MAC");
   const modifier = isMac ? e.metaKey : e.ctrlKey;
   if (modifier && e.key === "Enter") showMessage();
